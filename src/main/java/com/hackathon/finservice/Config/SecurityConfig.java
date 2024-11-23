c5fe7ed12323d6dcb3c24aa9a64f43db552ca657
jgarcia6 <jgarcia6@werfen.com>
2024-11-23 21:09:15 +0100

    Task 2 User register

--- src/main/java/com/hackathon/finservice/Config/SecurityConfig.java
+++ src/main/java/com/hackathon/finservice/Config/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,35 +15,35 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//    http.csrf(csrf -> csrf
-//        .ignoringRequestMatchers("/health", "/api/users/register")
-//        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()));
-//
-//    http.headers(headers -> headers
-//        .contentSecurityPolicy(contentSecurityPolicyConfig -> contentSecurityPolicyConfig.policyDirectives(
-//            "default-src 'self'; script-src 'self' 'unsafe-inline'; object-src 'none';")));
-//
-//    http.headers(headers -> headers.frameOptions(FrameOptionsConfig::sameOrigin));
-//
-//    http.cors(Customizer.withDefaults())
-//        .authorizeHttpRequests(authorize -> authorize
-//            .requestMatchers(HttpMethod.GET, "/health").permitAll()
-//            .requestMatchers(HttpMethod.POST, "/api/users/register").permitAll()
-//            .anyRequest().authenticated()
-//        );
-//
-//    return http.build();
+    http.csrf(csrf -> csrf
+        .ignoringRequestMatchers("/health", "/api/users/register")
+        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()));
 
-    http
-        .headers(headers -> headers.frameOptions(FrameOptionsConfig::disable))
-        .csrf(AbstractHttpConfigurer::disable)
-        .cors(Customizer.withDefaults())
+    http.headers(headers -> headers
+        .contentSecurityPolicy(contentSecurityPolicyConfig -> contentSecurityPolicyConfig.policyDirectives(
+            "default-src 'self'; script-src 'self' 'unsafe-inline'; object-src 'none';")));
+
+    http.headers(headers -> headers.frameOptions(FrameOptionsConfig::sameOrigin));
+
+    http.cors(Customizer.withDefaults())
         .authorizeHttpRequests(authorize -> authorize
             .requestMatchers(HttpMethod.GET, "/health").permitAll()
-            .requestMatchers("/api/users/register").permitAll()
+            .requestMatchers(HttpMethod.POST, "/api/users/register").permitAll()
             .anyRequest().authenticated()
         );
+
     return http.build();
+
+//    http
+//        .headers(headers -> headers.frameOptions(FrameOptionsConfig::disable))
+//        .csrf(AbstractHttpConfigurer::disable)
+//        .cors(Customizer.withDefaults())
+//        .authorizeHttpRequests(authorize -> authorize
+//            .requestMatchers(HttpMethod.GET, "/health").permitAll()
+//            .requestMatchers("/api/users/register").permitAll()
+//            .anyRequest().authenticated()
+//        );
+//    return http.build();
   }
 
   @Bean
