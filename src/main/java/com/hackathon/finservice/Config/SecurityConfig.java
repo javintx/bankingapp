6833f88414b0144c48b6255c9aa458dfb994a5c8
jgarcia6 <jgarcia6@werfen.com>
2024-11-25 17:07:32 +0100

    Task 2 User

--- src/main/java/com/hackathon/finservice/Config/SecurityConfig.java
+++ src/main/java/com/hackathon/finservice/Config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,21 +17,28 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     return http
-        .csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
-        .headers(headers -> headers
-            .contentSecurityPolicy(contentSecurityPolicyConfig -> contentSecurityPolicyConfig.policyDirectives(
-                "default-src 'self'; script-src 'self'; object-src 'none';"))
-            .frameOptions(FrameOptionsConfig::sameOrigin))
-        .cors(Customizer.withDefaults())
-        .authorizeHttpRequests(authorize -> authorize
-            .anyRequest().permitAll()
-//            .requestMatchers(HttpMethod.GET, "/health").permitAll()
-//            .requestMatchers(HttpMethod.POST, "/api/users/register").permitAll()
-//            .anyRequest().authenticated()
-        )
+        .csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
         .build();
   }
 
+//  @Bean
+//  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//    return http
+//        .csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+//        .headers(headers -> headers
+//            .contentSecurityPolicy(contentSecurityPolicyConfig -> contentSecurityPolicyConfig.policyDirectives(
+//                "default-src 'self'; script-src 'self'; object-src 'none';"))
+//            .frameOptions(FrameOptionsConfig::sameOrigin))
+//        .cors(Customizer.withDefaults())
+//        .authorizeHttpRequests(authorize -> authorize
+//            .anyRequest().permitAll()
+
+  /// /            .requestMatchers(HttpMethod.GET, "/health").permitAll() /
+  /// .requestMatchers(HttpMethod.POST, "/api/users/register").permitAll() /            .anyRequest().authenticated()
+//        )
+//        .build();
+//  }
   @Bean
   public BCryptPasswordEncoder bCryptPasswordEncoder() {
     return new BCryptPasswordEncoder(12);
