ab952370cbb8743faec23cf83e64bc8b91b1d01e
jgarcia6 <jgarcia6@werfen.com>
2024-12-03 15:37:54 +0100

    Task 5

--- src/main/java/com/hackathon/finservice/Config/SecurityConfig.java
+++ src/main/java/com/hackathon/finservice/Config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.hackathon.finservice.Config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -13,6 +15,21 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+  private final CustomUserDetailsService userDetailsService;
+
+  @Autowired
+  public SecurityConfig(CustomUserDetailsService userDetailsService) {
+    this.userDetailsService = userDetailsService;
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(HttpSecurity http, PasswordEncoder passwordEncoder)
+      throws Exception {
+    var authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+    authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+    return authenticationManagerBuilder.build();
+  }
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
